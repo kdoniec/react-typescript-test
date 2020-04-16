b6daf5a601488301b423a8d25b7648a5afbc0417
Krzysztof Doniec <kdoniec@gmail.com>
2020-04-16 23:24:17 +0200

    Usage bootstrap

--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,63 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
+import NavbarTop from './NavbarTop';
 import * as serviceWorker from './serviceWorker';
 import Button from 'react-bootstrap/Button';
 import Jumbotron from 'react-bootstrap/Jumbotron';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
-    <Jumbotron>
-      <h1>Hello, world!</h1>
-      <p>
-        This is a simple hero unit, a simple jumbotron-style component for calling
-        extra attention to featured content or information.
-      </p>
-      <p>
-        <Button variant="primary">Learn more</Button>
-      </p>
-    </Jumbotron>
+    <NavbarTop />
+
+    <main role="main">
+      <Jumbotron>
+        <Container>
+        <h1>Hello, world!</h1>
+        <p>
+          This is a simple hero unit, a simple jumbotron-style component for calling
+          extra attention to featured content or information.
+        </p>
+        <p>
+          <Button variant="primary">Learn more</Button>
+        </p>
+        </Container>
+      </Jumbotron>
+
+      <Container>
+        <Row>
+           <Col>
+              <h2>Heading</h2>
+              <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+              <Button variant="secondary">Learn more</Button>
+           </Col>
+           <Col>
+              <h2>Heading</h2>
+              <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+              <Button variant="primary">Learn more</Button>
+           </Col>
+           <Col>
+              <h2>Heading</h2>
+              <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+              <Button variant="primary">Learn more</Button>
+           </Col>
+        </Row>
+        <hr/>
+      </Container>
+    </main>
+
+    <footer>
+      <Container>
+        <p>© Company 2017-2018</p>
+      </Container>
+    </footer>
+
   </React.StrictMode>,
   document.getElementById('root')
 );
